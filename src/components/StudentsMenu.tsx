07dfcb13ea1031a9335b4d7548d5247153fe17c9
Mouli-Maharaj <142859363+mouli1508@users.noreply.github.com>
2024-11-08 09:51:45 -0500

    changed calendar

--- src/components/StudentsMenu.tsx
+++ src/components/StudentsMenu.tsx
@@ -62,7 +62,7 @@ const StudentMenu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
+          <span className="hidden lg:block text-white font-light my-4">
             {i.title}
           </span>
           {i.items.map((item) => {
@@ -71,7 +71,7 @@ const StudentMenu = () => {
                 <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                  className="flex items-center justify-center lg:justify-start gap-4 text-white py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
                 >
                   <Image src={item.icon} alt="" width={20} height={20} />
                   <span className="hidden lg:block">{item.label}</span>
